bf6922fec170ca14c3cbf5346f969889599c196f
ChainarongN <36422853+ChainarongN@users.noreply.github.com>
2020-01-15 16:05:03 +0700

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ComponentsComponent } from './components/components.component';
+import { CateIotComponent } from './components/cate-iot/cate-iot.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'index', pathMatch: 'full' },
   { path: 'index', component: ComponentsComponent },
+  { path: 'cate-iot', component: CateIotComponent },
 ];
 
 @NgModule({
