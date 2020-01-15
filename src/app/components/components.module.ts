fe28679297590ead6dc8db6390b2f486bd1ffd1c
ChainarongN <36422853+ChainarongN@users.noreply.github.com>
2020-01-15 16:06:44 +0700

    Update components.module.ts

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -9,13 +9,14 @@ import { TopbarComponent } from './topbar/topbar.component';
 import { HeaderComponent } from './header/header.component';
 import { IntroComponent } from './intro/intro.component';
 import { AboutComponent } from './about/about.component';
-import { ServiceComponent } from './service/service.component';
+import { ServiceComponent } from './product/service.component';
 import { ClientsComponent } from './clients/clients.component';
 import { OutPortfolioComponent } from './out-portfolio/out-portfolio.component';
 import { TestimonialsComponent } from './testimonials/testimonials.component';
 import { CallToActionComponent } from './call-to-action/call-to-action.component';
 import { OurTeamComponent } from './our-team/our-team.component';
 import { ContactComponent } from './contact/contact.component';
+import { CateIotComponent } from './cate-iot/cate-iot.component';
 
 @NgModule({
     imports: [
@@ -37,6 +38,7 @@ import { ContactComponent } from './contact/contact.component';
         CallToActionComponent,
         OurTeamComponent,
         ContactComponent,
+        CateIotComponent,
     ],
     exports: [ComponentsComponent]
 })
