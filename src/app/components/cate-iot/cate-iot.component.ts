d536cc9459b4a435c69d0c566fb90d27cde9124a
ChainarongN <36422853+ChainarongN@users.noreply.github.com>
2020-01-15 16:05:30 +0700

    Create cate-iot.component.ts

---
+++ src/app/components/cate-iot/cate-iot.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cate-iot',
+  templateUrl: './cate-iot.component.html',
+  styleUrls: ['./cate-iot.component.css']
+})
+export class CateIotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
