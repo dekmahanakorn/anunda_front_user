12a3bcb6d01acf2d85c7c127c750548ef60e37b9
ChainarongN <36422853+ChainarongN@users.noreply.github.com>
2020-01-16 13:27:35 +0700

    Create cate-rf-passive.component.ts

---
+++ src/app/components/cate-rf-passive/cate-rf-passive.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cate-rf-passive',
+  templateUrl: './cate-rf-passive.component.html',
+  styleUrls: ['./cate-rf-passive.component.css']
+})
+export class CateRfPassiveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
