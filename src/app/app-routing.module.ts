fc8408621c122b3018e92320b21687c9f4fa1c5c
ChainarongN <36422853+ChainarongN@users.noreply.github.com>
2020-01-18 14:23:08 +0700

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { CateIotComponent } from './components/cate-iot/cate-iot.component';
 import { CateRfMicrowaveComponent } from './components/cate-rf-microwave/cate-rf-microwave.component';
 import { CateRfPassiveComponent } from './components/cate-rf-passive/cate-rf-passive.component';
 import { CateKioskComponent } from './components/cate-kiosk/cate-kiosk.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 
 
 const routes: Routes = [
@@ -14,6 +15,7 @@ const routes: Routes = [
   { path: 'cate-rf-microwave', component: CateRfMicrowaveComponent },
   { path: 'cate-rf-passive', component: CateRfPassiveComponent },
   { path: 'cate-kiosk', component: CateKioskComponent },
+  { path: 'product-detail', component: ProductDetailComponent },
 ];
 
 @NgModule({
